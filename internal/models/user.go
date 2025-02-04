8a895edcf753a4cb4e27a5eafe572c8e3b319c68
Md Yousuf Ali <yousuf3usb@gmail.com>
2025-02-04 21:13:46 +0600

    feat: update user table schema

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,14 +2,15 @@ package models
 
 import (
 	"database/sql"
-	"gorm.io/gorm"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type User struct {
 	ID          uint           `json:"id" gorm:"primaryKey"`
-	Name        string         `json:"name" binding:"required"`
-	Email       *string        `json:"email" binding:"required,email"`
+	Name        string         `json:"name" gorm:"type:varchar(100);not null" binding:"required"`
+	Email       string         `json:"email" gorm:"type:varchar(100);not null" binding:"required,email"`
 	Birthday    *time.Time     `json:"birthday"`
 	ActivatedAt sql.NullTime   `json:"activated_at"`
 	CreatedAt   time.Time      `json:"created_at"`
